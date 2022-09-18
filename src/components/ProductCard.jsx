d81a8b7a9f572561204fda87f1d12e3e910c8ed0
Geras4323 <german432yo@gmail.com>
2022-09-18 19:43:55 -0300

    [CLASE 19] Traer productos desde API

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function ProductCard({price, title}) {
+function ProductCard({price, title, image}) {
   const [onCart, setOnCart] = React.useState([]);
 
   const handleClick = () => {
@@ -9,7 +9,7 @@ function ProductCard({price, title}) {
 
   return (
     <div className="w-36   sm:w-60">
-      <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" className="w-36 h-36 rounded-2xl object-cover   sm:w-60 sm:h-60" />
+      <img src={image} className="w-36 h-36 rounded-2xl object-cover   sm:w-60 sm:h-60" />
       <div className="flex justify-between items-center mt-3">
         <div>
           <p className="font-bold text-md mt-0 mb-1">{price}</p>
--- src/containers/ProductsList.jsx
+++ src/containers/ProductsList.jsx
@@ -1,18 +1,36 @@
+import axios from 'axios';
 import React from 'react';
 
 import { ProductCard } from '../components/ProductCard';
 
+const API = 'https://api.escuelajs.co/api/v1/products?limit=30&offset=0';
+
 function ProductsList() {
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchAPI() {
+      const response = await fetch(API)
+      const data = await response.json()
+      setProducts(data)
+    }
+
+    fetchAPI()
+  }, [])
+
+
   return (
-    <section className="mt-8">
+    <section className="my-8">
       <div className="grid grid-cols-autosm gap-6 place-content-center   sm:grid-cols-automd">
 
-        <ProductCard price="$ 120,00" title="Bike"/>
-        <ProductCard price="$ 120,00" title="Bike"/>
-        <ProductCard price="$ 120,00" title="Bike"/>
-        <ProductCard price="$ 120,00" title="Bike"/>
-        <ProductCard price="$ 120,00" title="Bike"/>
-        <ProductCard price="$ 120,00" title="Bike"/>
+        {products.map(product => (
+          <ProductCard
+            key={product.id}
+            price={`$ ${product.price}`}
+            title={product.title}
+            image={product.images[0]}
+          />
+        ))}
 
       </div>
     </section>
